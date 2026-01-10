818ef61582f18abd8c2d1dec0f541ea91b0031ba
Surendar Balaji <sbalaji07@icloud.com>
2026-01-10 17:11:47 +0000

    the commit for testing

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main(void) {
     printf("Hello, World!\n");
     printf("Good day, How do you do fellow programmers\n");
+    printf("Changes must be made until this project is detected. Perhaps there is something I forgot to set up");
     return 0;
 }
